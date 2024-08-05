0d15a7f3142a1ca049927ee5a316613c3906daea
Aryadani10rpl7 <aryadani_santoso_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:38:56 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,13 @@
+public class Data {
+    public static void main(String[] args) {
+        String nama = "Arya" ;
+        int usia = 16;
+        String alamat = "Jl.cemara no 97 Blitar" ;
+        double kelas = 10.7;
+
+        System.out.println("nama saya : " + nama);
+        System.out.println("usia saya : " + usia);
+        System.out.println("alamat : " + alamat);
+        System.out.println("kelas : " + kelas);
+    }
+}
